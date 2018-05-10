705981e40faf8da33d697d5c83b6d1fc9da3a4ee
MaryAnne45 <mary.jereh@gmail.com>
2018-05-10 11:20:14 +0300

    added a login template

--- Instagram/views.py
+++ Instagram/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/accounts/login')
 def index(request):
     title='Welcome to instagram'
     return render(request,'main/index.html',{"title":title})
