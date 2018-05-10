b17e0c6f85947f12485415829da95a230b64a989
MaryAnne45 <mary.jereh@gmail.com>
2018-05-10 10:26:33 +0300

    created a view function for the home page

--- Instagram/views.py
+++ Instagram/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    title='Welcome to instagram'
+    return render(request,'main/index.html',{"title":title})
--- instagram_project/settings.py
+++ instagram_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Instagram.apps.InstagramConfig',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
