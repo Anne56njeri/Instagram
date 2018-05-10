45017367655f40f8d808eeaedfb243cead9bb7de
MaryAnne45 <mary.jereh@gmail.com>
2018-05-10 11:11:40 +0300

    added authentication system

--- Instagram/models.py
+++ Instagram/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    name=models.CharField(max_length=30)
+    Bio= models.TextField()
+    user=models.ForeignKey(User)
--- Instagram/urls.py
+++ Instagram/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 from .import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
 url(r'^$',views.index,name='Index'),
+url(r'^accounts/',include('registration.backends.simple.urls')),
 ]
