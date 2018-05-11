9100d336bc7ce84ac37b1cfa3569e65f98b299f5
MaryAnne45 <mary.jereh@gmail.com>
2018-05-11 12:36:47 +0300

    added a view function

--- Instagram/models.py
+++ Instagram/models.py
@@ -7,10 +7,18 @@ class Profile(models.Model):
     Bio= models.TextField()
     profile_image=models.ImageField(upload_to = 'profiles/',blank=True)
     user=models.ForeignKey(User)
-    
+    def save_profile(self):
+        self.save()
+    def delete_profile(self):
+        self.delete()
+
 
 class Image(models.Model):
     name=models.CharField(max_length=20)
     image_caption=models.CharField(max_length=1000,blank=True)
-    profile=models.ForeignKey(Profile)
+    profile=models.ForeignKey(Profile,null=True)
     image_path=models.ImageField(upload_to='images/',blank=True)
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()    
--- Instagram/views.py
+++ Instagram/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .forms import ProfileForm,ImageForm
-from .models import Profile
+from .models import Profile,Image
+from django.http import Http404
 # Create your views here.
 @login_required(login_url='/accounts/login')
 def index(request):
     title='Welcome to instagram'
     profile_info =Profile.objects.all()
-    images_prof=Images.objects.all()
+    images_prof=Image.objects.all()
     return render(request,'main/index.html',{"title":title,"profile_info":profile_info,"images_prof":images_prof})
 def first_profile(request):
     current_user=request.user
@@ -29,6 +30,13 @@ def add_image(request):
         form=ImageForm(request.POST,request.FILES)
         if form.is_valid():
             form.save()
+            return redirect(index)
     else:
         form=ImageForm()
     return render(request,'main/image.html',{"form":form})
+def details(request,image_id):
+    try:
+        image_details = Image.objects.get(id=image_id)
+    except DoesNotExsist:
+        raise Http404()
+    return render(request,'main/details.html',{"image_details":image_details})
