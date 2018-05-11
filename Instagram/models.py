098d05cb2dfeca450b57ac757d40796711fc0c51
MaryAnne45 <mary.jereh@gmail.com>
2018-05-11 10:37:31 +0300

    updating user profile information

--- Instagram/models.py
+++ Instagram/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.dispatch import receiver
 # Create your models here.
 class Profile(models.Model):
     name=models.CharField(max_length=30)
     Bio= models.TextField()
     profile_image=models.ImageField(upload_to = 'profiles/',blank=True)
     user=models.ForeignKey(User)
+    
 
 class Image(models.Model):
     name=models.CharField(max_length=20)
--- Instagram/views.py
+++ Instagram/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-from .forms import ProfileForm
+from .forms import ProfileForm,ImageForm
 from .models import Profile
 # Create your views here.
 @login_required(login_url='/accounts/login')
@@ -10,15 +10,24 @@ def index(request):
     return render(request,'main/index.html',{"title":title,"profile_info":profile_info})
 def first_profile(request):
     current_user=request.user
+    user=Profile.objects.get(user_id=current_user)
     if request.method == 'POST':
-        form =ProfileForm(request.POST,request.FILES,instance=request.user)
+        form =ProfileForm(request.POST,request.FILES,instance=user)
         if form.is_valid():
-            profile=form.save(commit=False)
-            profile.user=current_user
-            profile.save()
+            first_profile=form.save(commit=False)
+            first_profile.user=current_user
+            first_profile.save()
             return redirect(index)
     else:
         form = ProfileForm()
 
 
     return render(request,'main/profile.html',{"form":form})
+def add_image(request):
+    if request.method == 'POST':
+        form=ImageForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+    else:
+        form=ImageForm()
+    return render(request,'main/image.html')
